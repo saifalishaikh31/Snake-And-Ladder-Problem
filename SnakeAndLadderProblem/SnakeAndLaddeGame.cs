aff978aa1c4e878b1e6175620a4604db61f8203f
saifalishaikh31 <saifalishaikh31>
2022-01-28 11:09:55 +0530

    [Saif Ali] Add: Player Reaches Exact winning position 100 Added

--- SnakeAndLadderProblem/SnakeAndLaddeGame.cs
+++ SnakeAndLadderProblem/SnakeAndLaddeGame.cs
@@ -8,35 +8,34 @@ namespace SnakeAndLadderProblem
     class SnakeAndLaddeGame
     {
         const int NOPLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION=100;
-        int player_pos = 0;
+        int player_pos = 0, count=0;
         Random random = new Random();
         public int DiceRoll()
         {
+            count++;
             return random.Next(1, 7);
         }
        
         public void Play()
         {
-            while (this.player_pos < WINNING_POSITION)
+            while (this.player_pos != WINNING_POSITION)
             {
                 int option = random.Next(0, 3);
                 switch (option)
                 {
                     case NOPLAY:
-                        Console.WriteLine("The player stays in the same position");
+                        //Console.WriteLine("The player stays in the same position");
                         break;
                     case LADDER:
                         if(this.player_pos + DiceRoll() < 100)
                         {
                             this.player_pos += DiceRoll();
-                            Console.WriteLine("The player moves ahead by the number of position received in the die : " + player_pos);
                         }
                         break;
                     case SNAKE:
                         if (this.player_pos + DiceRoll() >= 0)
                         {
                             this.player_pos -= DiceRoll();
-                            Console.WriteLine("The player moves behind by the number of position received in the die : " + player_pos);
                         }
                         else
                         {
@@ -46,7 +45,9 @@ namespace SnakeAndLadderProblem
                     default:
                         break;
                 }
+                Console.WriteLine("Player position"+ this.player_pos);
             }
+            Console.WriteLine("Number of time Dice Rolled"+ count);
            
         }
     }
