5b187097fb62f9b650e583ebc39d1b14de533313
saifalishaikh31 <saifalishaikh31>
2022-01-28 11:03:00 +0530

    [Saif Ali] Add: Player Reaches the winning position 100 Added

--- SnakeAndLadderProblem/SnakeAndLaddeGame.cs
+++ SnakeAndLadderProblem/SnakeAndLaddeGame.cs
@@ -7,33 +7,45 @@ namespace SnakeAndLadderProblem
 {
     class SnakeAndLaddeGame
     {
-        const int NOPLAY = 0, LADDER = 1, SNAKE = 2;
+        const int NOPLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION=100;
         int player_pos = 0;
         Random random = new Random();
         public int DiceRoll()
         {
             return random.Next(1, 7);
         }
-        
+       
         public void Play()
         {
-            int option = random.Next(0, 3);
-            switch (option)
+            while (this.player_pos < WINNING_POSITION)
             {
-                case NOPLAY:
-                    Console.WriteLine("The player stays in the same position");
-                    break;
-                case LADDER:
-                    
-                    this.player_pos += DiceRoll();
-                    Console.WriteLine("The player moves ahead by the number of position received in the die : " + player_pos);
-                    break;
-                case SNAKE:
-                    this.player_pos -= DiceRoll();
-                    Console.WriteLine("The player moves behind by the number of position received in the die : " + player_pos);
-                    break;
-                default:
-                    break;
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NOPLAY:
+                        Console.WriteLine("The player stays in the same position");
+                        break;
+                    case LADDER:
+                        if(this.player_pos + DiceRoll() < 100)
+                        {
+                            this.player_pos += DiceRoll();
+                            Console.WriteLine("The player moves ahead by the number of position received in the die : " + player_pos);
+                        }
+                        break;
+                    case SNAKE:
+                        if (this.player_pos + DiceRoll() >= 0)
+                        {
+                            this.player_pos -= DiceRoll();
+                            Console.WriteLine("The player moves behind by the number of position received in the die : " + player_pos);
+                        }
+                        else
+                        {
+                            this.player_pos = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
            
         }
