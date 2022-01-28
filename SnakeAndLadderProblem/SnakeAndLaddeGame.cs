dd6c43669252231f937079339cf4cb5e0f8100b8
saifalishaikh31 <saifalishaikh31>
2022-01-28 10:19:15 +0530

    [Saif Ali] Add: Player Position Start start at 0 Added

---
+++ SnakeAndLadderProblem/SnakeAndLaddeGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeAndLadderProblem
+{
+    class SnakeAndLaddeGame
+    {
+        public void Play()
+        {
+            int player_pos = 0;
+        }
+    }
+}
