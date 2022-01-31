399a1e7da68a2522038918a4453bd8f64e292e39
saifalishaikh31 <saifalishaikh31>
2022-01-31 08:30:23 +0530

    [Saif Ali] Add: Play With Two Player Added

--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -9,8 +9,47 @@ namespace SnakeAndLadderProblem
     {
         static void Main(string[] args)
         {
-            SnakeAndLaddeGame game = new SnakeAndLaddeGame();
-            game.Play();
+            const int WINNING_POSITION = 100;
+            
+            SnakeAndLaddeGame player1 = new SnakeAndLaddeGame();
+            SnakeAndLaddeGame player2 = new SnakeAndLaddeGame();
+            Random random = new Random();
+            int option = random.Next(0, 2);
+            bool turn = true;
+            if (option == 0)
+                turn = true;
+            if (option == 1)
+                turn = false;
+            int player1Position = 0, player2Position = 0;
+            bool flag = true;
+            while (flag)
+            {
+                if (turn)
+                {
+                    player1Position = player1.Play();
+                    turn = false;
+                    Console.WriteLine("Player1 Position : " + player1Position);
+                }
+                else
+                {
+                    player2Position = player2.Play();
+                    turn = true;
+                    Console.WriteLine("Player2 Position : " + player2Position);
+                }
+                
+
+                if (player1Position == WINNING_POSITION)
+                {
+                    Console.WriteLine("Player1 won the game! \n");
+                    flag = false;
+                }
+                if (player2Position == WINNING_POSITION)
+                {
+                    Console.WriteLine("Player2 won the game! \n");
+                    flag = false;
+                }
+            
+            }
             
             Console.ReadLine();
         }
--- SnakeAndLadderProblem/SnakeAndLaddeGame.cs
+++ SnakeAndLadderProblem/SnakeAndLaddeGame.cs
@@ -7,9 +7,8 @@ namespace SnakeAndLadderProblem
 {
     class SnakeAndLaddeGame
     {
-        const int NOPLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100, POSITION = 0;
-        int player1_pos = 0, player2_pos = 0, count = 0;
-        
+        const int NOPLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
+        int player_pos = 0, count = 0;
         Random random = new Random();
         public int DiceRoll()
         {
@@ -17,12 +16,9 @@ namespace SnakeAndLadderProblem
             return random.Next(1, 7);
         }
 
-
-
-        public void Play()
+        public int Play()
         {
-            while (this.player1_pos != WINNING_POSITION && this.player2_pos != WINNING_POSITION)
-            {
+            
                 int option = random.Next(0, 3);
                 switch (option)
                 {
@@ -30,75 +26,28 @@ namespace SnakeAndLadderProblem
                         //Console.WriteLine("The player stays in the same position");
                         break;
                     case LADDER:
-                        if (this.player1_pos + DiceRoll() < 100)
+                        if (this.player_pos + DiceRoll() < 100)
                         {
-                            Console.WriteLine("player 1 position : " + this.player1_pos);
-                            this.player1_pos += DiceRoll();
-                            if (this.player1_pos + DiceRoll() < 100)
-                            {
-                                Console.WriteLine("player 1 play again");
-                                this.player1_pos += DiceRoll();
-                                Console.WriteLine("player 1 position : " + this.player1_pos);
-                            }
-                           
+                            this.player_pos += DiceRoll();
                         }
-                        
-                        if (this.player2_pos + DiceRoll() < 100)
-                        {
-                            Console.WriteLine("player 2 position : " + this.player2_pos);
-                            this.player2_pos += DiceRoll();
-                            if (this.player2_pos + DiceRoll() < 100)
-                            {
-                                Console.WriteLine("player 2 play again");
-                                this.player2_pos += DiceRoll(); ;
-                                Console.WriteLine("player 2 position : " + this.player2_pos);
-                            }
-                          
-                        }
-                        
                         break;
-
                     case SNAKE:
-
-                        if (this.player1_pos + DiceRoll() >= 0)
+                        if (this.player_pos + DiceRoll() >= 0)
                         {
-
-                            this.player1_pos -= DiceRoll();
-                            Console.WriteLine("player 1 position : " + this.player1_pos);
-
+                            this.player_pos -= DiceRoll();
                         }
                         else
                         {
-                            this.player1_pos = 0;
-                            Console.WriteLine("player 1 position : " + this.player1_pos);
-                        }
-
-                        if (this.player2_pos + DiceRoll() >= 0)
-                        {
-                            this.player2_pos -= DiceRoll();
-                            Console.WriteLine("player 2 position : " + this.player2_pos);
-                        }
-                        else
-                        {
-                            this.player2_pos = 0;
-                            Console.WriteLine("player 2 position : " + this.player2_pos);
+                            this.player_pos = 0;
                         }
                         break;
                     default:
                         break;
 
                 }
+            return player_pos;
 
-            } 
-            Console.WriteLine("Number of time Dice Rolled" + count);
-            if (player1_pos == WINNING_POSITION)
-            {
-                Console.WriteLine("Player1 Has Won The Game!!!");
-            }
-            else
-            {
-                Console.WriteLine("Player2 Has Won The Game!!!");
-            }
         }
+        
     }
 }
